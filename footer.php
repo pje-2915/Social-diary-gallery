bf9d172bd8b1c599ac88dea944ff2753f3379f44
Paul Edwards <pje@pauls-mbp.lan>
2016-05-02 20:03:19 +0100

    Adding some forgotten footer items

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 	<footer class="site-footer">
 		<div class="site-footer-div">
-			<h2>&copy <?php bloginfo('name'); ?> - <?php echo date('Y'); ?><h2>
+		<p>
+		&copy <?php bloginfo('name'); ?> <?php echo date('Y'); ?>
+		<a href="http://www.facebook.com/groups/4865359982"> <img src="<?php bloginfo('template_directory');?>/img/facebook.jpg" width="100" height="26" alt="fb"></a>
+		<a href="http://www.ramblers.co.uk/" target="_blank"> <img src="<?php bloginfo('template_directory');?>/img/ramble.jpg" alt="ramble"></a>&nbspPart of The Ramblers Association.
+		</p>
 		</div>
 	</footer>
 

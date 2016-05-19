182e5727991e07c344363d3de2dcf343caa64370
Paul Edwards <pje@Pauls-MacBook-Pro.local>
2016-05-19 12:26:55 +0100

    Add forgotten description for posts

--- archive-Walk.php
+++ archive-Walk.php
@@ -48,6 +48,12 @@ $query = new WP_Query( $args );
 				echo '<p>No content found</p>';
 			endif;
 		?>
+		<hr color=grey width=95%>
+		<h4>Description of Walk Grading System</h4><ul>
+<li><b>Easy:</b> Short walks (6 miles or less) on good quality paths with no difficult terrain. These walks should be suitable for anybody in their 20s or 30s who doesn't have a mobility disability and are particularly suitable for people who have done little walking before.</li>
+<li><b>Leisurely:</b> Slightly more difficult walks which represent the majority of walks we undertake. These walks may be longer (up to 12 miles) and while the paths will still generally be good, a few sections may be steep, muddy, rough or overgrown, so boots or good quality walking shoes are recommended. These walks should still be suitable for the large majority of people in our age group.</li>
+<li><b>Moderate:</b> More energetic walks still, either because they are long (>12 miles), have significant hills, are likely to be very muddy or overgrown or because a faster pace is expected. Walking boots are strongly recommended. Suitable for reasonably fit people with some walking experience, but may not be a good choice for your first walk with us if you haven't done much walking before.</li>
+<li><b>Strenuous:</b> Very long walks or those covering very hilly terrain, suitable for experienced walkers of above average fitness. If in doubt about your fitness, contact the leader beforehand. These walks are unusual on our main programme, but may feature in some of our weekends away.</li></ul>
 	</div>
 	<div class="resourcecolumn">
 	<?php get_sidebar(); ?>
--- category-weekends-away.php
+++ category-weekends-away.php
@@ -6,7 +6,7 @@ get_header();
 <section class="midpage">
 	<div class="blogcolumn">
 	
-		<p>Weekends away are organized several times each year.
+		<p><b>Weekends away are organized several times each year.
 		These usually involve several walks on successive days, 
 		a group meal or pub visit, and hostel-type accommodation.  
 		They are only open to paid-up members of the Ramblers, 
@@ -14,7 +14,7 @@ get_header();
 		published in our weekly e-mail newsletter, not on our 
 		website or our Facebook or Meetup pages.  
 		This page shows just a small selection of images and 
-		reports from our past weekends away.</p>
+		reports from our past weekends away.</b></p>
 		<?
 		if (have_posts()) :
 			while (have_posts()) : the_post(); ?>
